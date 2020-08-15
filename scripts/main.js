9d9736b732c4338a2845e6a3e6837321efe41d31
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-08-15 19:10:03 -0400

    Bug fixed

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Howdy howdy, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Howdy howdy, ' + storedName;
+}
+
+myButton.onclick = function() {
+  console.log("Hello World")
+  setUserName();
+}
